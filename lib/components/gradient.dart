ed196099d365aae619ceb6c380fc72f21052f00f
Utkarsh Jha <jha.utkarsh2005@gmail.com>
2026-02-19 21:41:59 +0530

    gr

--- lib/components/gradient.dart
+++ lib/components/gradient.dart
@@ -1,19 +1,42 @@
+// import 'package:flutter/material.dart';
+
+// class GradientBackground extends StatelessWidget {
+//   final Widget child;
+//   const GradientBackground({super.key, required this.child});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       width: double.infinity,
+//       padding: const EdgeInsets.symmetric(horizontal: 24),
+//       decoration: const BoxDecoration(
+//         gradient: LinearGradient(
+//           begin: Alignment.topCenter,
+//           end: Alignment.bottomCenter,
+//           colors: [Color(0xFFEFF4FF), Color(0xFF9893FF)],
+//         ),
+//       ),
+//       child: child,
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 
 class GradientBackground extends StatelessWidget {
   final Widget child;
+
   const GradientBackground({super.key, required this.child});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      padding: const EdgeInsets.symmetric(horizontal: 24),
+      height: double.infinity,
       decoration: const BoxDecoration(
         gradient: LinearGradient(
+          colors: [Color(0xFFF8F9FF), Color(0xFFEFF1FF)],
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
-          colors: [Color(0xFFEFF4FF), Color(0xFF9893FF)],
         ),
       ),
       child: child,
