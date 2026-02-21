7766ab0cd20b6e673ef1067bfbecf0702e081405
Utkarsh Jha <jha.utkarsh2005@gmail.com>
2026-02-21 12:47:25 +0530

    loco

--- lib/Home.dart
+++ lib/Home.dart
@@ -67,7 +67,7 @@ class _HomeState extends State<Home> {
                   const SizedBox(height: 30),
 
                   _settingsItem(Icons.person, "Profile", Colors.blue),
-                  _settingsItem(Icons.lock, "Privacy", Colors.orange),
+                  _settingsItem(Icons.location_on, "Location", Colors.orange),
                   _settingsItem(Icons.help_outline, "Help", Colors.purple),
                   _settingsItem(
                     Icons.delete_outline,
