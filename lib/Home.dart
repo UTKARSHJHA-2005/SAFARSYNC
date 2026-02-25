5df1d30a6708e35134594bbed90a1657a58696fc
Utkarsh Jha <jha.utkarsh2005@gmail.com>
2026-02-25 12:11:27 +0530

    rout

--- lib/Home.dart
+++ lib/Home.dart
@@ -167,25 +167,6 @@ class _HomeState extends State<Home> {
     );
   }
 
-  Route _createRoute() {
-    return PageRouteBuilder(
-      pageBuilder: (context, animation, secondaryAnimation) =>
-          const FullMapPage(),
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        const begin = Offset(0.0, 1.0);
-        const end = Offset.zero;
-        const curve = Curves.easeInOut;
-
-        var tween = Tween(
-          begin: begin,
-          end: end,
-        ).chain(CurveTween(curve: curve));
-
-        return SlideTransition(position: animation.drive(tween), child: child);
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     void _handleMapInteraction() {
