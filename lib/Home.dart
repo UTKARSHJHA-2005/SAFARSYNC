3bfe6c86d55d99a9bda3b9aeb33b055bdac144d2
Utkarsh Jha <jha.utkarsh2005@gmail.com>
2026-02-25 12:34:23 +0530

    SOS//

--- lib/Home.dart
+++ lib/Home.dart
@@ -642,94 +642,94 @@ class _HomeState extends State<Home> {
     );
   }
 
-  void showEmergencySheet(BuildContext context) {
-    showModalBottomSheet(
-      context: context,
-      isScrollControlled: true,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
-      ),
-      builder: (context) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                height: 5,
-                width: 50,
-                margin: const EdgeInsets.only(bottom: 20),
-                decoration: BoxDecoration(
-                  color: Colors.black26,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              const Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Report Emergency",
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
-                ),
-              ),
-              const SizedBox(height: 20),
-              _emItem(Icons.gavel, Colors.red, "Armed Robbery"),
-              _emItem(Icons.sensor_door_outlined, Colors.yellow, "Break In"),
-              _emItem(
-                Icons.local_fire_department,
-                Colors.orange,
-                "Fire Outbreak",
-              ),
-              _emItem(Icons.local_hospital, Colors.green, "Medical Emergency"),
-              _emItem(Icons.visibility, Colors.purple, "Suspicious Activity"),
-              _emItem(Icons.call, Colors.blue, "Other Emergency"),
-              const SizedBox(height: 30),
-              Row(
-                children: [
-                  Expanded(
-                    child: OutlinedButton(
-                      onPressed: () => Navigator.pop(context),
-                      child: const Text("Cancel"),
-                    ),
-                  ),
-                  const SizedBox(width: 12),
-                  Expanded(
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue,
-                      ),
-                      onPressed: () {},
-                      child: const Text("Proceed"),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-            ],
-          ),
-        );
-      },
-    );
-  }
+  // void showEmergencySheet(BuildContext context) {
+  //   showModalBottomSheet(
+  //     context: context,
+  //     isScrollControlled: true,
+  //     shape: const RoundedRectangleBorder(
+  //       borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+  //     ),
+  //     builder: (context) {
+  //       return Padding(
+  //         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+  //         child: Column(
+  //           mainAxisSize: MainAxisSize.min,
+  //           children: [
+  //             Container(
+  //               height: 5,
+  //               width: 50,
+  //               margin: const EdgeInsets.only(bottom: 20),
+  //               decoration: BoxDecoration(
+  //                 color: Colors.black26,
+  //                 borderRadius: BorderRadius.circular(10),
+  //               ),
+  //             ),
+  //             const Align(
+  //               alignment: Alignment.centerLeft,
+  //               child: Text(
+  //                 "Report Emergency",
+  //                 style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+  //               ),
+  //             ),
+  //             const SizedBox(height: 20),
+  //             _emItem(Icons.gavel, Colors.red, "Armed Robbery"),
+  //             _emItem(Icons.sensor_door_outlined, Colors.yellow, "Break In"),
+  //             _emItem(
+  //               Icons.local_fire_department,
+  //               Colors.orange,
+  //               "Fire Outbreak",
+  //             ),
+  //             _emItem(Icons.local_hospital, Colors.green, "Medical Emergency"),
+  //             _emItem(Icons.visibility, Colors.purple, "Suspicious Activity"),
+  //             _emItem(Icons.call, Colors.blue, "Other Emergency"),
+  //             const SizedBox(height: 30),
+  //             Row(
+  //               children: [
+  //                 Expanded(
+  //                   child: OutlinedButton(
+  //                     onPressed: () => Navigator.pop(context),
+  //                     child: const Text("Cancel"),
+  //                   ),
+  //                 ),
+  //                 const SizedBox(width: 12),
+  //                 Expanded(
+  //                   child: ElevatedButton(
+  //                     style: ElevatedButton.styleFrom(
+  //                       backgroundColor: Colors.blue,
+  //                     ),
+  //                     onPressed: () {},
+  //                     child: const Text("Proceed"),
+  //                   ),
+  //                 ),
+  //               ],
+  //             ),
+  //             const SizedBox(height: 20),
+  //           ],
+  //         ),
+  //       );
+  //     },
+  //   );
+  // }
 
-  Widget _emItem(IconData icon, Color color, String label) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 10),
-      child: Row(
-        children: [
-          CircleAvatar(
-            radius: 22,
-            backgroundColor: color.withOpacity(0.2),
-            child: Icon(icon, color: color, size: 22),
-          ),
-          const SizedBox(width: 15),
-          Text(
-            label,
-            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-          ),
-        ],
-      ),
-    );
-  }
+  // Widget _emItem(IconData icon, Color color, String label) {
+  //   return Padding(
+  //     padding: const EdgeInsets.symmetric(vertical: 10),
+  //     child: Row(
+  //       children: [
+  //         CircleAvatar(
+  //           radius: 22,
+  //           backgroundColor: color.withOpacity(0.2),
+  //           child: Icon(icon, color: color, size: 22),
+  //         ),
+  //         const SizedBox(width: 15),
+  //         Text(
+  //           label,
+  //           style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
 
   Widget _squareCard({
     required String title,
@@ -912,21 +912,21 @@ class _HomeState extends State<Home> {
     );
   }
 
-  Widget _sosButton(BuildContext context) {
-    return GestureDetector(
-      onTap: () => showEmergencySheet(context),
-      child: const CircleAvatar(
-        radius: 40,
-        backgroundColor: Colors.redAccent,
-        child: Text(
-          "SOS",
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 22,
-          ),
-        ),
-      ),
-    );
-  }
+  // Widget _sosButton(BuildContext context) {
+  //   return GestureDetector(
+  //     onTap: () => showEmergencySheet(context),
+  //     child: const CircleAvatar(
+  //       radius: 40,
+  //       backgroundColor: Colors.redAccent,
+  //       child: Text(
+  //         "SOS",
+  //         style: TextStyle(
+  //           color: Colors.white,
+  //           fontWeight: FontWeight.bold,
+  //           fontSize: 22,
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
 }
