6b6dc739785e80eab43aa9e11a577593a4f6965f
Utkarsh Jha <jha.utkarsh2005@gmail.com>
2026-02-26 23:40:49 +0530

    filereg

--- lib/model/user_register.dart
+++ lib/model/user_register.dart
@@ -1,1 +1,27 @@
+class UserRegistration {
+  String? name;
+  String? gender;
+  int? age;
+  String? country;
+  String? phone;
 
+  String? photoCID;
+
+  String? summary;
+  List<String>? interests;
+  List<Map<String, String>>? emergencyContacts;
+
+  Map<String, dynamic> toJson() {
+    return {
+      "name": name,
+      "gender": gender,
+      "age": age,
+      "country": country,
+      "phone": phone,
+      "photo": photoCID,
+      "summary": summary,
+      "interests": interests,
+      "emergencyContacts": emergencyContacts,
+    };
+  }
+}
