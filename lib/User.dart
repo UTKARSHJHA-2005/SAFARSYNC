d014e0460c880a42da49bd09b005e75cdd64f6f7
Utkarsh Jha <jha.utkarsh2005@gmail.com>
2026-02-28 21:10:52 +0530

    crypto

--- lib/User.dart
+++ lib/User.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:safarsync/components/gradient.dart';
 import 'dart:convert';
-import 'package:crypto/crypto.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
