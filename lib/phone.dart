17e500b3d858dff083118941d0a3c2135d6f1db5
Utkarsh Jha <jha.utkarsh2005@gmail.com>
2026-02-28 23:23:28 +0530

    htt

--- lib/phone.dart
+++ lib/phone.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:safarsync/Register.dart';
 import 'package:safarsync/otp.dart';
 
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 class PhoneInputScreen extends StatefulWidget {
   const PhoneInputScreen({super.key});
 
