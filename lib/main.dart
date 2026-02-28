0efc568e371c9a4c41522b66b0d4461690909edd
Utkarsh Jha <jha.utkarsh2005@gmail.com>
2026-02-28 23:21:34 +0530

    mai

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,23 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
+  Future<void> checkLogin() async {
+    final prefs = await SharedPreferences.getInstance();
+    final isLoggedIn = prefs.getBool("isLoggedIn") ?? false;
+
+    if (isLoggedIn) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const ProfilePage()),
+      );
+    } else {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const PhoneInputScreen()),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(title: 'Flutter Demo', home: const PhoneInputScreen());
