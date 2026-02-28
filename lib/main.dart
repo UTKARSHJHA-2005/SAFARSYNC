9f43d6614e765c889e331e0aed208c5bac9b8fae
Utkarsh Jha <jha.utkarsh2005@gmail.com>
2026-02-28 23:22:43 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:safarsync/phone.dart';
+import 'package:safarsync/user.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
