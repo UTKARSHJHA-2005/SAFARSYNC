016d5e8ddc311370215bfc105ffb31f9f3451f02
Utkarsh Jha <jha.utkarsh2005@gmail.com>
2026-03-01 21:23:08 +0530

    pgip2

--- lib/phone.dart
+++ lib/phone.dart
@@ -92,7 +92,7 @@ class _PhoneInputScreenState extends State<PhoneInputScreen> {
                       final phone = completePhoneNumber.trim();
                       final response = await http.get(
                         Uri.parse(
-                          "http://192.168.1.5:3000/verify-user/${Uri.encodeComponent(phone)}",
+                          "http://192.168.1.4:3000/verify-user/${Uri.encodeComponent(phone)}",
                         ),
                       );
 
