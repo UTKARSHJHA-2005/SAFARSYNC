d92b0fafbff8f0ba24f43e3e41d4ed108149de2a
Utkarsh Jha <jha.utkarsh2005@gmail.com>
2026-03-02 16:03:17 +0530

    helloimp

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:safarsync/Hello.dart';
 import 'package:safarsync/phone.dart';
-import 'package:safarsync/user.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
