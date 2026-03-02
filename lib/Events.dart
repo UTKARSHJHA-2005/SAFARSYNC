a4f9ad5236bde1495d2808c3c347bda972717171
Utkarsh Jha <jha.utkarsh2005@gmail.com>
2026-03-02 13:46:31 +0530

    ratdatafun

--- lib/Events.dart
+++ lib/Events.dart
@@ -222,6 +222,33 @@ class _EventsState extends State<Events> with TickerProviderStateMixin {
 
   List<VocalItem> _vocalData = [];
   bool _isVocalLoading = true;
+  Future<List<VocalItem>> fetchVocals() async {
+    final response = await http.get(
+      Uri.parse('http://192.168.1.3:3000/vocals'),
+    );
+
+    if (response.statusCode == 200) {
+      final List data = jsonDecode(response.body);
+      return data
+          .map(
+            (e) => VocalItem(
+              title: e['title'] ?? '',
+              location: e['location'] ?? '',
+              image: e['image'] ?? '',
+              category: e['category'] ?? '',
+              emoji: e['emoji'] ?? '',
+              date: e['date'] ?? '',
+              time: e['time'] ?? '',
+              organizer: e['organizer'] ?? '',
+              description: e['description'] ?? '',
+              price: double.tryParse(e['price'].toString()) ?? 0.0,
+            ),
+          )
+          .toList();
+    } else {
+      throw Exception('Failed to load vocals');
+    }
+  }
 
   // static const List<StayItem> _stayData = [
   //   StayItem(
