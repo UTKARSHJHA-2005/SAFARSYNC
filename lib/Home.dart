a123b2af3d4c519aa39c5913eee59ef5de794070
Utkarsh Jha <jha.utkarsh2005@gmail.com>
2026-03-03 13:07:06 +0530

    locstate

--- lib/Home.dart
+++ lib/Home.dart
@@ -49,7 +49,9 @@ class _HomeState extends State<Home> {
 
   Future<void> fetchStateImages() async {
     final response = await http.get(
-      Uri.parse("http://192.168.1.3:3000/state-images/${widget.selectedState}"),
+      Uri.parse(
+        "http://192.168.1.3:3000/state-images/${Uri.encodeComponent(widget.selectedState)}",
+      ),
     );
 
     if (response.statusCode == 200) {
