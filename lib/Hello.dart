7e47a076177f7cd5ca181c177f7322998176e121
Utkarsh Jha <jha.utkarsh2005@gmail.com>
2026-03-03 14:32:50 +0530

    Notif

--- lib/Hello.dart
+++ lib/Hello.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:safarsync/State.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class Hello extends StatelessWidget {
   const Hello({super.key});
